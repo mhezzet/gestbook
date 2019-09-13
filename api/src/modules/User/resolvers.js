55da25cfcda50b57a804342aa62e6d5043f47aa0
mhezzet <40916821+mhezzet@users.noreply.github.com>
2019-09-13 10:45:51 +0200

    adp: added signin endpoint

--- api/src/modules/User/resolvers.js
+++ api/src/modules/User/resolvers.js
@@ -1,9 +1,7 @@
 import { UserInputError, AuthenticationError } from 'apollo-server'
-import { signupValidator } from './validation'
+import { signupValidator, signinValidator } from './validation'
 
 async function signup(_, args, { models: { User } }) {
-  console.log(args)
-
   //validated inout schema
   await signupValidator(args)
 
@@ -20,8 +18,27 @@ async function signup(_, args, { models: { User } }) {
   return { user, token }
 }
 
+async function signin(_, args, { models: { User } }) {
+  //validated inout schema
+  await signinValidator(args)
+
+  //check email existance
+  const user = await User.findOne({ email: args.email })
+  if (!user) throw new AuthenticationError('invalid email or password')
+
+  //check password correctness
+  const validPassword = await user.validPassword(args.password)
+  if (!validPassword) throw new AuthenticationError('invalid email or password')
+
+  //generate token
+  const token = user.genToken()
+
+  return { user, token }
+}
+
 export default {
   Mutation: {
-    signup
+    signup,
+    signin
   }
 }
