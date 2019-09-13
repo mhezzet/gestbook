8126a9894a23b45e412317ae1c45cba06f4f20b4
mhezzet <40916821+mhezzet@users.noreply.github.com>
2019-09-13 11:34:53 +0200

    api: added create post endpoint

--- api/src/modules/Post/resolvers.js
+++ api/src/modules/Post/resolvers.js
@@ -1,7 +1,40 @@
 import { UserInputError, AuthenticationError, ApolloError } from 'apollo-server'
-import { GraphQLScalarType } from 'graphql'
+import { createPostValidator } from './validation'
+
+async function createPost(
+  _,
+  args,
+  { models: { Post, User }, user: requester }
+) {
+  //validate the input schema
+  await createPostValidator(args)
+
+  //create post
+  const post = await Post.create({ ...args, user: requester.id })
+
+  console.log(post)
+
+  //add the post to user instance
+  const user = await User.findOneAndUpdate(
+    { _id: requester.id },
+    {
+      $push: { posts: post._id }
+    },
+    {
+      new: true
+    }
+  )
+  console.log(user)
+
+  return post
+}
+
+async function posts(_, __, { models: { Post } }) {
+  const posts = await Post.find()
+  return posts
+}
 
 export default {
-  Mutation: {},
-  Query: {}
+  Mutation: { createPost },
+  Query: { posts }
 }
